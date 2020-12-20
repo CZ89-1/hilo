84304474153d4c64ed67eb5fdc954309dadb2af7
sun <sun@LAPTOP-U9TTM40V>
2020-12-20 19:17:04 +0800

    result

---
+++ src/main/java/com/yc/hilo/po/Result.java
@@ -0,0 +1,51 @@
+package com.yc.hilo.po;
+
+public class Result implements java.io.Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private int code;
+	private String msg;
+	private Object data;
+	
+	public static Result success(String msg) {
+		return new Result(1,msg);
+	}
+	
+	public static Result failure(String msg) {
+		return new Result(0,msg);
+	}
+	
+	public Result() {
+		
+	}
+	
+	public Result(int code, String msg) {
+		this.code = code;
+		this.msg = msg;
+	}
+	
+	public Result(int code, String msg, Object data) {
+		this.code = code;
+		this.msg = msg;
+		this.data = data;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+	
+}
